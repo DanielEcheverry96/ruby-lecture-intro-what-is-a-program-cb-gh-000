162eca031b5eaecf761136a90f9dc570729da307
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-21 04:26:46 +0000

    Done.

--- lib/a_ruby_program.rb
+++ lib/a_ruby_program.rb
@@ -1,12 +1,13 @@
 # What is a Program?
 
+puts "Hello world from Ruby"
 puts "This is a program"
 puts "Programs are interpreted at runtime."
 puts "Which means even though I'm writing this Tue Jan 17 2012 8:36am"
 puts "the time now is "+ Time.now.to_s
 
 puts "Programs have flow."
-puts "Which means that you see this first"
+puts "Which means that you see ./Hellothis first"
 puts "and this next."
 
 puts "Lines of code are executed linearly, that's flow."
@@ -19,5 +20,3 @@ puts "Literal pieces of data like this very sentence (or String)."
 puts "Finally, barewords, or variables, that are set equal to things."
 
 puts "Anything that isn't one of those will cause an error."
-
-see
